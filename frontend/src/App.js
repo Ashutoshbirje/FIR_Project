f787027eb86dbec0c1c98ca260abe5d25f725ee4
Ashutoshbirje <ashutoshbirje4@gmail.com>
2025-04-26 01:15:44 +0530

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useRef} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Navbar from "./Components/Navbar/Navbar";
 import Instruction from "./Components/Instruction/Instruction"; 
 import HomePage from "./Components/HomePage/HomePage";
 import Advertisement from "./Components/Advertisement/Advertisement";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Footer from "./Components/Footer/Footer";
-import LoginSignup from "./Components/LoginSignup/LoginSignup"; // New component
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
 
@@ -17,6 +17,16 @@ function App() {
     return !!localStorage.getItem("token");
   });
 
+  // Sync login state if token is removed from other tabs
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setIsLoggedIn(!!localStorage.getItem("token"));
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, []);
+
   const scrollToSection = (sectionRef) => {
     if (sectionRef.current) {
       sectionRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -25,23 +35,35 @@ function App() {
 
   return (
     <div className="container">
-      {
-        isLoggedIn ? (
-          <>
-            <Instruction />
-            <div ref={section2Ref}></div>
-            <div className="sticky-navbar">
-              <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} scrollToSection={scrollToSection} refs={{ section1Ref,section2Ref }} />
+      {isLoggedIn ? (
+        <>
+          <Instruction />
+          <div ref={section2Ref}></div>
+
+          <div className="sticky-navbar">
+            <Navbar 
+              isLoggedIn={isLoggedIn} 
+              setIsLoggedIn={setIsLoggedIn} 
+              scrollToSection={scrollToSection} 
+              refs={{ section1Ref, section2Ref }} 
+            />
+          </div>
+
+          <HomePage />
+          <Advertisement />
+
+          {/* Protect Dashboard - just in case */}
+          {localStorage.getItem("token") ? (
+            <div className="Dashboard" ref={section1Ref}>
+              <Dashboard />
             </div>
-            <HomePage />
-            <Advertisement />
-            <div className="Dashboard"ref={section1Ref}><Dashboard/></div>
-            <Footer />
-          </>
-        ) : (
-          <LoginSignup onLogin={() => setIsLoggedIn(true)} />
-        )
-      }
+          ) : null}
+
+          <Footer />
+        </>
+      ) : (
+        <LoginSignup onLogin={() => setIsLoggedIn(true)} />
+      )}
     </div>
   );
 }

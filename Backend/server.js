591c996ddadb0b24d251d04a43ae7156126b2636
Ashutoshbirje <ashutoshbirje4@gmail.com>
2025-07-29 22:31:01 +0530

    ok

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,6 @@ dotenv.config();
 const authRoutes = require("./routes/authRoutes");
 const firRoutes = require("./routes/firRoutes");
 const reportRoutes  = require("./routes/reportRoutes");
-const Predict = require("./routes/predictiRoutes");
 
 const app = express();
 
@@ -18,7 +17,6 @@ app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/api", firRoutes);
 app.use("/api", reportRoutes);
-app.use("/api", Predict);
 
 // Connect to MongoDB and start server
 mongoose
